9b19f5212e22b4c9f180de06ac02e44ceb744031
Daniel Svensson <daniel.svensson3@ikea.com>
2017-10-03 19:49:24 +0200

    Cleaned up and turned off sessions

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='GrisVakt',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='GrisVakt will download images from a Gmail-account and genereate a web-site. Used to present images taken by a ScoutGuard Trail-cam.',
+      author='Daniel Svensson',
+      author_email='hyttinge@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
      )
